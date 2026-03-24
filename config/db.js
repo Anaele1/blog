9577e92502f48d259a232ac4936aa953db41f291
Anaele1 <anaeleprofit@gmail.com>
2026-03-24 07:48:53 -0700

    db mode switch

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 exports.connectDB = async () => {
     try {
-        // const conn = await mongoose.connect(process.env.MongoUrlLocal) // Offline connection string
+        //const conn = await mongoose.connect(process.env.MongoUrlLocal) // Offline connection string
         const conn = await mongoose.connect(process.env.MongoUrlOnline) // Online connection string
         console.log(`mongoDB connected: ${conn.connection.host}`)
     } catch (error) {
