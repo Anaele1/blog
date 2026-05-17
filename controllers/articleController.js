b22787fa53e3f6522031540beacfb69341a7b3f1
Anaele1 <anaeleprofit@gmail.com>
2026-05-17 07:41:27 -0700

    routing and UI edit

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -6,7 +6,7 @@ const { userDetailsOnTemplate } = require("../utils/templateConsistency");
 // Create post
 exports.createPost = async (req, res) => {
     const user = userDetailsOnTemplate(req.user);
-    const {writerId} = req.body;
+    const { writerId } = req.body;
     const { title, description, keywrds, content, summary } = req.body;
     try {
         if (!title || !description || !keywrds || !content || !summary) {
@@ -36,7 +36,7 @@ exports.getCreatePost = async (req, res) => {
     try {
         res.render('posts/createPost', { title: 'Create Post', metaTitle: 'create-post', user });
     } catch (error) {
-        console.log(error);  
+        console.log(error);
     }
 }
 
@@ -51,55 +51,56 @@ exports.eachUserPostsView = async (req, res) => {
         //return res.status(201).json({Message: 'POSTS', result}) 
     } catch (error) {
         console.log(error);
-        return res.status(401).json('failled to retrieve all posts')   
+        return res.status(401).json('failled to retrieve all posts')
     }
 }
 
 // Retrieve posts
 exports.getAllPosts = async (req, res) => {
     try {
+        const user = userDetailsOnTemplate(req.user);
         const allPost = await articleModel.find().populate('writerId');
         const results = allPost.map(writer => writer.toObject());
         //const results = allPost;
-        return res.render('posts/postFeeds', { results});
+        return res.render('posts/postFeeds', { results, user });
         //return res.status(201).json({Message: 'POSTS', result}) 
     } catch (error) {
         console.log(error);
-        return res.status(401).json('failled to retrieve all posts')   
+        return res.status(401).json('failled to retrieve all posts')
     }
 };
 
 // Retrieve posts
 exports.userViewEacPostInDetails = async (req, res) => {
     let workId = req.params._id.replace(/^:|:$/g, '').trim();;
-console.log('This-work-id',workId);
+    console.log('This-work-id', workId);
 
     try {
         const eachPost = await articleModel.findById(workId).populate('writerId').lean();
         const results = eachPost;
         //console.log(results);
-        
-        return res.render('posts/viewPosts', { results})
+
+        return res.render('posts/viewPosts', { results })
         //return res.status(201).json({Message: 'POSTS', result}) 
     } catch (error) {
         console.log(error);
-        return res.status(401).json('Error getting this work in details')   
+        return res.status(401).json('Error getting this work in details')
     }
 };
 
 // Update Post Contents
 exports.updateContent = async (req, res) => {
-    const {id} = req.params;
-    const {content} = req.body;
+    const { id } = req.params;
+    const { content } = req.body;
 
     try {
-        if (!content){
-            return res.status(401).json('Not a content field')  
+        if (!content) {
+            return res.status(401).json('Not a content field')
         }
 
-        const contentBody = await articleModel.findByIdAndUpdate(id, {content})
+        const contentBody = await articleModel.findByIdAndUpdate(id, { content })
         const result = contentBody
-        return res.status(200).json({Message: 'Content updated successfully', result})
+        return res.status(200).json({ Message: 'Content updated successfully', result })
     } catch (error) {
         console.log(error);
         return res.status(401).json('failled to update content')
@@ -108,26 +109,27 @@ exports.updateContent = async (req, res) => {
 
 // Get Posts for a particular user
 exports.postsByAWriter = async (req, res) => {
-   try {
+    try {
+        const user = userDetailsOnTemplate(req.user)
         // Sanitize the writerId (remove any extra characters)
         let writerId = req.params.id;
         writerId = writerId.replace(/^:|:$/g, '').trim();
 
-         // Validate the ObjectId
+        // Validate the ObjectId
         if (!mongoose.Types.ObjectId.isValid(writerId)) {
             return res.status(400).json({ error: 'Invalid writer ID' });
         }
-        
+
         // Find all articles by the writer
         const postsPerwriter = await articleModel.find({ writerId }).populate('writerId', 'email username').lean();
-console.log('Details', postsPerwriter);
+        console.log('Details', postsPerwriter);
 
         if (!postsPerwriter || postsPerwriter.length === 0) {
             return res.status(404).json({ message: 'No posts found for this writer' });
         }
 
         const results = postsPerwriter;
-        return res.render('posts/myPosts', { results});
+        return res.render('posts/myPosts', { results, user });
         //res.status(200).json({ success: true, articles });
     } catch (err) {
         console.error(err);
